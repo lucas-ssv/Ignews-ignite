06818ff4810e827c3c23868c9a5e49d49a7bb4ed
lucas-ssv <lucas_99silva@hotmail.com>
2021-06-16 11:22:51 -0300

    Challenge ignews

---
+++ src/components/SubscribeButton/index.tsx
@@ -0,0 +1,29 @@
+import { session, signIn, useSession } from "next-auth/client";
+import styles from "./styles.module.scss";
+
+interface SubscribeButtonProps {
+  priceId: string;
+}
+
+export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [śession] = useSession();
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn("github");
+      return;
+    }
+
+    // criação da checkout session
+  }
+
+  return (
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
+      Subscribe now
+    </button>
+  );
+}

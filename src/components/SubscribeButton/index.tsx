1c0e4fe25a7287e74e2668c87b1a0ed3f0b68b6a
lucas-ssv <lucas_99silva@hotmail.com>
2021-07-18 20:45:03 -0300

    Project ignews with NextJS and NextAuth finished

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,4 +1,7 @@
-import { session, signIn, useSession } from "next-auth/client";
+import { signIn, useSession } from "next-auth/client";
+import { useRouter } from "next/router";
+import { api } from "../../services/api";
+import { getStripeJs } from "../../services/stripe-js";
 import styles from "./styles.module.scss";
 
 interface SubscribeButtonProps {
@@ -6,15 +9,32 @@ interface SubscribeButtonProps {
 }
 
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
-  const [śession] = useSession();
+  const [session] = useSession();
+  const router = useRouter();
 
-  function handleSubscribe() {
+  async function handleSubscribe() {
     if (!session) {
       signIn("github");
       return;
     }
 
+    if (session.activeSubscription) {
+      router.push("/posts");
+      return;
+    }
+
     // criação da checkout session
+    try {
+      const response = await api.post("/subscribe");
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({ sessionId });
+    } catch (err) {
+      alert(err.message);
+    }
   }
 
   return (

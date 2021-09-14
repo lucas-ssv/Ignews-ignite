b24f5c4050b68ebab0d287fa696a4f78fc418a9f
lucas-ssv <lucas_99silva@hotmail.com>
2021-09-14 17:07:18 -0300

    fix: Wait result response

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -58,9 +58,9 @@ export const getServerSideProps: GetServerSideProps = async ({
 
   const post = {
     slug,
-    title: RichText.asText(response.data.title),
-    content: RichText.asHtml(response.data.content),
-    updatedAt: new Date(response.last_publication_date).toLocaleDateString(
+    title: RichText.asText(response?.data.title),
+    content: RichText.asHtml(response?.data.content),
+    updatedAt: new Date(response?.last_publication_date).toLocaleDateString(
       "pt-BR",
       {
         day: "2-digit",
